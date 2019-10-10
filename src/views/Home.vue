eef744b8de1c76a31e104f31cc26418d3c74906b
Rico Wan <ricowan2007@gmail.com>
2019-10-10 16:04:12 +0800

    updated home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-    Home
+      <v-carousel
+      cycle
+      height="400"
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+      >
+        <v-sheet
+          :color="colors[i]"
+          height="100%"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">{{ slide }}</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <v-container fill-height>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-title>First</v-card-title>
+            <v-card-text>Hi there, I am using Whatsapp!</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-card-title>Second</v-card-title>
+            <v-card-text>Hi there, I am using Facebook!</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-card-title>Third</v-card-title>
+            <v-card-text>Hi there, I am using STEM Portal!</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -8,5 +52,21 @@
 
 export default {
   //
+  data: () => ({
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+      'red lighten-1',
+      'deep-purple accent-4'
+    ],
+    slides: [
+      'Welcome',
+      'To',
+      'STEM',
+      'Portal',
+      ':)'
+    ]
+  })
 }
 </script>
