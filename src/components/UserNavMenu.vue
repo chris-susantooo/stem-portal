017e6a589f04d8130d8ad200fbcc0b7288c30970
alvinchristopher <alvinchristopher.susanto@gmail.com>
2019-10-12 08:09:18 +0800

    Improved preloader

--- src/components/UserNavMenu.vue
+++ src/components/UserNavMenu.vue
@@ -72,7 +72,7 @@ export default {
     user: {
       type: Object,
       required: true
-    },
+    }
   },
   data: () => ({
     message: true,
--- src/views/layouts/Master.vue
+++ src/views/layouts/Master.vue
@@ -2,8 +2,8 @@
   <div class="wrapper">
     <transition name="fade">
       <div v-if="isLoading" class="preloader d-flex justify-center align-center defer-500">
-        <bounce-spinner />
-        <span class="text-center mt-12 ml-1" style="position: fixed;">Loading...</span>
+        <bounce-spinner :color="spinnerColors[animStep]"/>
+        <span class="text-center mt-12" style="position: fixed;">{{ 'Loading' + '.'.repeat(animStep) }}</span>
       </div>
     </transition>
     <v-header />
@@ -15,18 +15,28 @@
 <script>
 import VHeader from './Header.vue'
 import VFooter from './Footer.vue'
-import { BounceSpinner } from 'vue-spinners'
+import BounceSpinner from '@/components/BounceSpinner.vue'
+
+let animInterval = null
 
 export default {
   components: { VHeader, VFooter, BounceSpinner },
   mounted () {
-    this.$nextTick(() => {
-      window.onload = () => (this.isLoading = false)
-    })
+    window.onload = () => (this.isLoading = false)
+    animInterval = setInterval(() => (this.animStep = (this.animStep + 1) % 4), 1000)
   },
   data: () => ({
-    isLoading: true
-  })
+    isLoading: true,
+    spinnerColors: ['#6789ba', '#ffed78', '#d35656', '#347474'],
+    animStep: 0
+  }),
+  watch: {
+    isLoading () {
+      if (!this.isLoading) {
+        clearInterval(animInterval)
+      }
+    }
+  }
 }
 </script>
 
