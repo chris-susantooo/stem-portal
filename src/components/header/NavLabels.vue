0ff962ab39a3fc27e1231baedff0ecba00e2b86a
alvinchristopher <alvinchristopher.susanto@gmail.com>
2019-11-01 05:41:50 +0800

    Fixed display bug per user

--- src/components/header/NavLabels.vue
+++ src/components/header/NavLabels.vue
@@ -3,20 +3,20 @@
     <div v-if="!isLoggedIn">
       <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
       <router-link to="/login">Teachers</router-link>
-      <router-link to="/home/student">Students</router-link>
+      <router-link to="/login">Students</router-link>
       <router-link to="/login">Parents</router-link>
       <router-link to="/login">Discussion</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'student'">
         <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
-        <router-link to="/home/student">Students</router-link>
+        <router-link to="/">Home</router-link>
         <router-link to="/login">Games</router-link>
         <router-link to="/login">Courses</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'teacher'">
       <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
-      <router-link to="/login">Teachers</router-link>
-      <router-link to="/login">Materials</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/happy-corner">Teachers' Corner</router-link>
       <router-link to="/login">Discussion</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'parent'">
