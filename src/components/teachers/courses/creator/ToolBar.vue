dbef4597b1b8251a9cfb524cadcf2a991536cfd1
alvinchristopher <alvinchristopher.susanto@gmail.com>
2019-11-13 02:53:18 +0800

    Fixed bugs

--- src/components/teachers/courses/creator/ToolBar.vue
+++ src/components/teachers/courses/creator/ToolBar.vue
@@ -4,51 +4,45 @@
     <v-spacer />
     <div class="d-none d-sm-flex">
       <div class="text-center">
-        <v-menu offset-y dark
-          :open-on-hover="!(options.section === undefined && options.checkpoint === undefined && options.question === undefined)"
-          :open-on-click="false"
-        >
+        <v-menu offset-y dark :open-on-hover="chapterSelected && subItemSelected" :open-on-click="false">
           <template v-slot:activator="{ on }">
-            <v-btn text v-on="on" @click="options.chapter === undefined ? null : $emit('delete', 'chapter')">
+            <v-btn text v-show="chapterSelected || subItemSelected" v-on="on" @click="deleteChapter">
               <v-icon>mdi-delete-circle</v-icon>&nbsp;Delete
-              {{ !(options.section === undefined && options.checkpoint === undefined && options.question === undefined) ? '...' : '' }}
+              {{ chapterSelected && subItemSelected ? '...' : ' chapter' }}
             </v-btn>
           </template>
           <v-list>
             <v-list-item @click="$emit('delete', 'chapter')">
               <v-list-item-title>Chapter</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.section !== undefined" @click="$emit('delete', 'section')">
+            <v-list-item v-if="sectionSelected" @click="$emit('delete', 'section')">
               <v-list-item-title>Section</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.checkpoint !== undefined" @click="$emit('delete', 'checkpoint')">
+            <v-list-item v-if="checkPointSelected" @click="$emit('delete', 'checkpoint')">
               <v-list-item-title>Checkpoint</v-list-item-title>
             </v-list-item>
-            <v-list-item
-              v-if="options.chapter !== undefined && options.checkpoint !== undefined && options.question !== undefined"
-              @click="$emit('delete', 'question')"
-            >
+            <v-list-item v-if="questionSelected" @click="$emit('delete', 'question')">
               <v-list-item-title>Question</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-        <v-menu offset-y dark :open-on-hover="options.chapter !== undefined" :open-on-click="false">
+        <v-menu offset-y dark :open-on-hover="chapterSelected" :open-on-click="false">
           <template v-slot:activator="{ on }">
-            <v-btn text v-on="on" @click="options.chapter === undefined ? $emit('add', 'chapter') : null">
-              <v-icon>mdi-plus-circle</v-icon> &nbsp;{{ options.chapter !== undefined ? 'New...' : ' chapter' }}
+            <v-btn text v-on="on" @click="!chapterSelected ? $emit('add', 'chapter') : null">
+              <v-icon>mdi-plus-circle</v-icon> &nbsp;{{ chapterSelected ? 'New...' : ' chapter' }}
             </v-btn>
           </template>
           <v-list>
             <v-list-item @click="$emit('add', 'chapter')">
               <v-list-item-title>Chapter</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('add', 'section')">
+            <v-list-item v-if="chapterSelected" @click="$emit('add', 'section')">
               <v-list-item-title>Section</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('add', 'checkpoint')">
+            <v-list-item v-if="chapterSelected" @click="$emit('add', 'checkpoint')">
               <v-list-item-title>Checkpoint</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined && options.checkpoint !== undefined" @click="$emit('add', 'question')">
+            <v-list-item v-if="checkPointSelected" @click="$emit('add', 'question')">
               <v-list-item-title>Question</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -57,9 +51,12 @@
     </div>
     <div class="d-flex d-sm-none">
       <div class="text-center">
-        <v-menu offset-y dark :open-on-hover="options.chapter !== undefined" :open-on-click="false">
+        <v-menu offset-y dark
+          :open-on-hover="chapterSelected && subItemSelected"
+          :open-on-click="false"
+        >
           <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on" @click="options.chapter === undefined ? $emit('delete', 'chapter') : null">
+            <v-btn icon v-on="on" @click="deleteChapter">
               <v-icon>mdi-delete-circle</v-icon>
             </v-btn>
           </template>
@@ -67,37 +64,34 @@
             <v-list-item @click="$emit('delete', 'chapter')">
               <v-list-item-title>Chapter</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('delete', 'section')">
+            <v-list-item v-if="sectionSelected" @click="$emit('delete', 'section')">
               <v-list-item-title>Section</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('delete', 'checkpoint')">
+            <v-list-item v-if="checkPointSelected" @click="$emit('delete', 'checkpoint')">
               <v-list-item-title>Checkpoint</v-list-item-title>
             </v-list-item>
-            <v-list-item
-              v-if="options.chapter !== undefined && options.checkpoint !== undefined && options.question !== undefined"
-              @click="$emit('add', 'question')"
-            >
+            <v-list-item v-if="questionSelected" @click="$emit('delete', 'question')">
               <v-list-item-title>Question</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-        <v-menu offset-y dark :open-on-hover="options.chapter !== undefined" :open-on-click="false">
+        <v-menu offset-y dark :open-on-hover="chapterSelected" :open-on-click="false">
           <template v-slot:activator="{ on }">
-            <v-btn icon @click="options.chapter === undefined ? $emit('add', 'chapter') : null">
+            <v-btn icon @click="!chapterSelected ? $emit('add', 'chapter') : null">
               <v-icon>mdi-plus-circle</v-icon>
             </v-btn>
           </template>
           <v-list>
-            <v-list-item v-if="options.chapter === undefined" @click="$emit('add', 'chapter')">
+            <v-list-item @click="$emit('add', 'chapter')">
               <v-list-item-title>Chapter</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('add', 'section')">
+            <v-list-item v-if="chapterSelected" @click="$emit('add', 'section')">
               <v-list-item-title>Section</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined" @click="$emit('add', 'checkpoint')">
+            <v-list-item v-if="chapterSelected" @click="$emit('add', 'checkpoint')">
               <v-list-item-title>Checkpoint</v-list-item-title>
             </v-list-item>
-            <v-list-item v-if="options.chapter !== undefined && options.checkpoint !== undefined" @click="$emit('add', 'question')">
+            <v-list-item v-if="chapterSelected && options.checkpoint !== undefined" @click="$emit('add', 'question')">
               <v-list-item-title>Question</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -113,6 +107,31 @@ export default {
     options: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    chapterIsEmpty () {
+      return !this.options.chapters[this.options.chapter].sections.length
+    },
+    chapterSelected () {
+      return this.options.chapter !== undefined
+    },
+    sectionSelected () {
+      return this.options.section !== undefined && this.options.checkpoint === undefined
+    },
+    checkPointSelected () {
+      return this.options.checkpoint !== undefined
+    },
+    questionSelected () {
+      return this.options.question !== undefined
+    },
+    subItemSelected () {
+      return this.options.section !== undefined || this.options.question !== undefined
+    }
+  },
+  methods: {
+    deleteChapter () {
+      if (this.chapterSelected && !this.subItemSelected) this.$emit('delete', 'chapter')
     }
   }
 }
