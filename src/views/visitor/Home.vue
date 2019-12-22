2012a2885d79c9faaa30bf4999a9782e2e294e29
Rico Wan <ricowan2007@gmail.com>
2019-12-22 22:45:05 +0800

    updated Home page

--- src/views/visitor/Home.vue
+++ src/views/visitor/Home.vue
@@ -3,31 +3,33 @@
     <v-carousel
       continuous
       cycle
-      show-arrows-on-hover
+      :show-arrows="false"
+      hide-delimiters
       delimiter-icon="mdi-minus"
       height="70vh"
     >
       <v-carousel-item
-        v-for="(slide, i) in slides"
+        v-for="(item, i) in slides"
         :key="i"
+        :src="item.img"
       >
-        <v-sheet
-          :color="colors[i]"
-          height="100%"
-        >
+        <div class="carousel-filter">
           <v-row
             class="fill-height"
             align="center"
             justify="center"
           >
-            <div class="display-3">{{ slide }}</div>
+            <div class="display-3">
+              <h3 class="carousel-text">Welcome to STEM Portal!</h3>
+            </div>
           </v-row>
-        </v-sheet>
+        </div>
       </v-carousel-item>
     </v-carousel>
+    <br>
     <div class="container">
       <div class="row">
-        <p class="font-weight-bold">Recent News</p>
+        <h2 class="font-weight-bold">Recent News</h2>
       </div>
       <div class="row">
         <v-col>
@@ -57,20 +59,23 @@
 
 export default {
   data: () => ({
-    colors: [
-      'indigo',
-      'warning',
-      'pink darken-2',
-      'red lighten-1',
-      'deep-purple accent-4'
-    ],
     slides: [
-      'Welcome',
-      'To',
-      'STEM',
-      'Portal',
-      ':)'
+      { img: 'https://anabolicaces.com/wp-content/uploads/2017/12/Cityscape-FB-article.jpg' },
+      { img: 'https://jacobsmedia.com/wp-content/uploads/2016/07/mobile-apps-1.jpg' },
+      { img: 'https://www.commercialintegrator.com/wp-content/uploads/2019/09/Technology-fast-1400x650.jpeg' },
+      { img: 'https://joinpop.org/wp-content/uploads/2019/04/laboratory.jpg' }
     ]
   })
 }
 </script>
+
+<style scoped>
+.carousel-filter {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.carousel-text {
+  color: white;
+}
+</style>
