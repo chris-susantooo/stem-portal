8b8a531034b1a965503547e8ee915f2eea6861b9
Alvin <alvinchristopher.susanto@gmail.com>
2020-01-08 17:05:06 +0800

    Updated user registration function

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,9 @@
 import Axios from 'axios'
 
-export function registerUser ({ username, password }) {
-  return Axios.post('/api/register', {
+export function registerUser (username, password, email) {
+  return Axios.post('users', {
     username,
-    password
+    password,
+    email
   })
 }
