73e992f67fb0781eb2da281218898945889eaddf
Alvin <alvinchristopher.susanto@gmail.com>
2020-01-08 17:05:16 +0800

    Better registration

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="login-bg" class="wrapper">
+    <!-- page elements -->
     <div class="container d-flex justify-end">
       <v-card max-width="400" class="vertical-center pa-3">
         <!-- Tab headers -->
@@ -16,10 +17,10 @@
                 View more, learn more with your account @STEM Portal.
                 Don't have one? Get yours for free, <a @click="tabs = 1">here</a>.
               </v-card-text>
-              <v-form>
+              <v-form v-model="loginForm">
                 <v-container>
-                  <v-text-field v-model="username" label="Username" outlined @keydown.enter="login" autofocus dense/>
-                  <v-text-field v-model="password" label="Password" outlined @keydown.enter="login" type="password" dense/>
+                  <v-text-field v-model="username" :counter="20" :rules="rules.name" label="Username" outlined @keydown.enter="login" autofocus dense/>
+                  <v-text-field v-model="password" :counter="20" :rules="rules.password" label="Password" outlined @keydown.enter="login" type="password" dense/>
                 </v-container>
                 <v-card-actions>
                   <v-btn text @click="true">Reset Password</v-btn>
@@ -34,12 +35,12 @@
             <v-card flat>
               <v-card-title class="mb-3">Join STEM Portal, 100% free.</v-card-title>
               <v-card-text>Experience STEM in the way you've never tried. The community is waiting for you.</v-card-text>
-              <v-form>
+              <v-form v-model="registerForm" ref="register-form">
                 <v-container>
-                  <v-text-field v-model="username" label="Username" outlined autofocus dense/>
-                  <v-text-field v-model="password" label="Password" outlined type="password" dense/>
-                  <v-text-field v-model="confirmPassword" label="Re-enter password" outlined type="password" dense/>
-                  <v-text-field v-model="email" label="Email address" outlined @keydown.enter="register" dense/>
+                  <v-text-field v-model="username" :counter="20" :rules="rules.name" label="Username" outlined autofocus dense/>
+                  <v-text-field v-model="password" :counter="20" :rules="rules.password" label="Password" outlined type="password" dense/>
+                  <v-text-field v-model="confirmPassword" :counter="20" :rules="rules.confirm" label="Re-enter password" outlined type="password" dense/>
+                  <v-text-field v-model="email" :rules="rules.email" label="Email address" outlined @keydown.enter="register" dense/>
                 </v-container>
                 <v-card-actions>
                   <div class="flex-grow-1"></div>
@@ -51,6 +52,23 @@
         </v-tabs-items>
       </v-card>
     </div>
+    <!-- register success dialog -->
+    <v-dialog v-model="registerSuccess" max-width="300">
+      <v-card>
+        <v-card-title class="headline">Operation Successful!</v-card-title>
+        <div class="pt-3" />
+        <v-card-text>
+          Please check your email inbox for a verification email.
+        </v-card-text>
+        <v-card-text>
+          Follow the available instructions to complete the account registration.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text color="primary" @click="registerSuccess = false">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -58,27 +76,64 @@
 import { registerUser } from '../utils/http'
 
 export default {
-  data: () => ({
-    tabs: 0,
-    username: '',
-    password: '',
-    confirmPassword: '',
-    email: ''
-  }),
+  data () {
+    return {
+      tabs: 0,
+      username: '',
+      password: '',
+      confirmPassword: '',
+      email: '',
+      loginForm: true,
+      registerForm: true,
+      rules: {
+        name: [
+          v => !!v || 'This field is required',
+          v => (v.length >= 6 && v.length <= 20) || 'Must be between 6 to 20 characters',
+          v => v.toLowerCase() === v || 'Must be all lowercase letters'
+        ],
+        password: [
+          v => !!v || 'This field is required',
+          v => (v.length >= 8 && v.length <= 20) || 'Must be between 8 to 20 characters'
+          // more validation rules TODO
+        ],
+        confirm: [
+          v => v === this.password || 'Must match the above password',
+          v => !!v || 'This field is required'
+        ],
+        email: [
+          v => !!v || 'Email is required',
+          v => /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Must be valid email'
+        ]
+      },
+      registerSuccess: false
+    }
+  },
   methods: {
     login () {
-      this.$store.dispatch('login', {
-        username: this.username,
-        password: this.password
-      }).then(() => {
-        this.$router.push({ name: 'home' })
-      }).catch(err => console.log(err))
+      if (this.loginForm) {
+        this.$store.dispatch('login', {
+          username: this.username,
+          password: this.password
+        }).then(() => {
+          this.$router.push({ name: 'home' })
+        }).catch(err => console.log(err))
+      }
     },
     register () {
-      registerUser({
-        username: this.username,
-        password: this.password
-      })
+      if (this.registerForm) {
+        registerUser(this.username, this.password, this.email)
+          .then(({ status, data }) => {
+            if (status === 201) {
+              this.username = ''
+              this.password = ''
+              this.confirmPassword = ''
+              this.email = ''
+              this.$refs['register-form'].resetValidation()
+              this.registerSuccess = true
+            }
+          })
+          .catch(err => console.log(err))
+      }
     }
   }
 }
