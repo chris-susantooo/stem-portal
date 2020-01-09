1a056b92bc15f7710029352da2666f34564cc01f
alvinchristopher <alvinchristopher.susanto@gmail.com>
2020-01-10 05:30:18 +0800

    Fixed discussion forum link

--- src/components/header/NavLabels.vue
+++ src/components/header/NavLabels.vue
@@ -5,26 +5,26 @@
       <router-link to="/teacher">Teachers</router-link>
       <router-link to="/student">Students</router-link>
       <router-link to="/parent">Parents</router-link>
-      <router-link to="/discussionforum">Discussion</router-link>
+      <router-link to="/discussion-forum">Discussion</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'student'">
         <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
         <router-link to="/">Home</router-link>
         <router-link to="/student/games">Games</router-link>
         <router-link to="/student/course">Courses</router-link>
-        <router-link to="/discussionforum">Discussion</router-link>
+        <router-link to="/discussion-forum">Discussion</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'teacher'">
       <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
       <router-link to="/">Home</router-link>
       <router-link to="/happy-corner">Teachers' Circle</router-link>
       <router-link to="/teacher/course">Courses</router-link>
-      <router-link to="/discussionforum">Discussion</router-link>
+      <router-link to="/discussion-forum">Discussion</router-link>
     </div>
     <div v-else-if="isLoggedIn && user.type === 'parent'">
       <a href="https://i.cs.hku.hk/fyp/2019/fyp19030/">About</a>
       <router-link to="/login">Discover</router-link>
-      <router-link to="/discussionforum">Discussion</router-link>
+      <router-link to="/discussion-forum">Discussion</router-link>
     </div>
   </div>
 </template>
