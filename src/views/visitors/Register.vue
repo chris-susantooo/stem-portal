2d002022f93a24c3b193ae51c92217649c9cf650
Rico Wan <ricowan2007@gmail.com>
2020-01-10 08:12:50 +0800

    postRegistration, checkEmail done

--- src/views/visitors/Register.vue
+++ src/views/visitors/Register.vue
@@ -2,8 +2,59 @@
   <div class="register-page">
     <div class="container">
       <div class="mb-5 text-center">
-        <h1>{{ header }}</h1>
+        <v-card v-if="username" flat>
+          <v-card-title class="mb-3">{{ header }}</v-card-title>
+          <v-card-text>Please provide us some information for further registration.</v-card-text>
+          <v-form v-model="postRegistrationForm">
+            <v-container>
+              <v-row>
+                <v-select v-model="role" :items="roles" :rules="rules.select" label="Your role" outlined dense/>
+              </v-row>
+              <v-row>
+                <v-text-field v-model="firstName" :counter="20" :rules="rules.name" label="First Name" outlined dense/>
+                <v-text-field v-model="lastName" :counter="20" :rules="rules.name" label="Last Name" outlined dense/>
+                <v-select v-model="gender" :items="genders" :rules="rules.select" label="Gender" outlined dense/>
+              </v-row>
+              <v-row>
+                <v-text-field v-model="school" :counter="50" :rules="rules.school" label="School Name" outlined dense/>
+              </v-row>
+              <v-row>
+                <v-select v-model="interest" :items="interests" :rules="rules.select" deletable-chips clearable multiple label="Area of Interests" outlined dense/>
+              </v-row>
+            </v-container>
+            <v-card-actions>
+              <div class="flex-grow-1"></div>
+              <v-btn color="primary" text @click="postRegistrationUser">Continue</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
       </div>
+      <!-- update user info error dialog -->
+      <v-dialog v-model="errorDialog" max-width="500">
+        <v-card class="pt-3">
+          <v-card-title class="headline mb-3">Something went wrong...</v-card-title>
+          <v-card-text>
+            There is an error at the moment. Please try your input and try again later!
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text color="primary" @click="errorDialog = false">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- update user info success dialog -->
+      <v-dialog v-model="updateSuccess" max-width="500">
+        <v-card class="pt-3">
+          <v-card-title class="headline mb-3">You have completed your registration!</v-card-title>
+          <v-card-text>
+            Try to log into STEM Portal and start your journey in STEM Education!
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text color="primary" :to="{ name: 'login' }">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
   </div>
 </template>
@@ -23,20 +74,62 @@ export default {
     const { token, cancel } = this.$route.query
     http.activateUser(username, token, cancel)
       .then(({ data: { status, user } }) => {
-        console.log(status, user)
         if (status === 'cancelled') this.header = 'Token invalidated! Thank you for helping us out'
         if (status === 'user verified') {
           this.header = 'Continue your registration here to use your account'
-          this.user = user
+          this.username = username
+          this.postRegistrationForm = true
         }
       })
       .catch(err => { console.log(err) })
       .finally(() => { this.$emit('childReady') })
   },
-  data: () => ({
-    user: null,
-    header: 'Failed to verify user token'
-  })
+  data () {
+    return {
+      username: '',
+      header: 'Failed to verify user token',
+      postRegistrationForm: false,
+      role: '',
+      firstName: '',
+      lastName: '',
+      gender: '',
+      school: '',
+      interest: [],
+      roles: ['Teacher', 'Student', 'Parent'],
+      genders: ['Male', 'Female', 'Others'],
+      interests: ['Science', 'Technology', 'Engineering', 'Mathematics'],
+      rules: {
+        name: [
+          v => !!v || 'This field is required',
+          v => (v.length >= 1 && v.length <= 20) || 'Must be between 6 to 20 characters',
+          v => /^([^0-9]*)$/.test(v) || 'Must not contain numbers'
+        ],
+        select: [
+          v => !!v || 'This field is required'
+        ],
+        school: [
+          v => !!v || 'This field is required',
+          v => (v.length >= 6 && v.length <= 50) || 'Must be between 6 to 50 characters'
+        ]
+      },
+      updateSuccess: false,
+      errorDialog: false
+    }
+  },
+  methods: {
+    postRegistrationUser () {
+      if (this.postRegistrationForm) {
+        http.postRegistrationUser(this.username, this.role, this.firstName, this.lastName, this.gender, this.school, this.interest)
+          .then(({ status, data }) => {
+            if (status === 200) this.updateSuccess = true
+          })
+          .catch(err => {
+            console.log(err)
+            this.errorDialog = true
+          })
+      }
+    }
+  }
 }
 </script>
 
