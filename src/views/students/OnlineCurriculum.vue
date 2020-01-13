511a5dcbb1dbab1e1dbac61b720527e93c58a04a
Rico Wan <ricowan2007@gmail.com>
2020-01-14 03:12:29 +0800

    update student view online course

--- src/views/students/OnlineCurriculum.vue
+++ src/views/students/OnlineCurriculum.vue
@@ -5,6 +5,23 @@
       <p>Choose an online course and start to learn something interesting!</p>
       <br>
     </div>
+    <h3 class="d-flex justify-center">Ongoing Online Courses</h3>
+    <template v-if="ongoingCourses.length>0">
+      <v-container>
+        <v-row class="d-flex justify-center">
+          <div class="mx-2" v-for="course in ongoingCourses" :key="course.id">
+            <course-card :course="course" :role="user.type" />
+          </div>
+        </v-row>
+      </v-container>
+    </template>
+    <template v-else>
+      <v-container>
+        <v-row class="d-flex justify-center">
+          <p>Opps! It seems you do not have any ongoing courses. Choose one and start to learn!</p>
+        </v-row>
+      </v-container>
+    </template>
     <h3 class="mb-n8 d-flex justify-center">Available Online Courses</h3>
     <v-container>
       <v-row class="d-flex justify-center">
@@ -29,9 +46,12 @@ export default {
   created () {
     this.$http.get('courses')
       .then(({ data: courses }) => { this.courses = courses })
+    this.$http.get(`courses?student=${this.$store.getters.user.username}`)
+      .then(({ data: courses }) => { this.ongoingCourses = courses })
   },
   data: () => ({
-    courses: []
+    courses: [],
+    ongoingCourses: []
   })
 }
 </script>
