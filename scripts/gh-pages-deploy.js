802141a3ae4ee72ea312022e33ae1af48e66150e
alvinchristopher <alvinchristopher.susanto@gmail.com>
2020-05-09 11:35:06 +0800

    uses yarn to build

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -5,7 +5,7 @@ const fs = require('fs');
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
     console.log('Building...')
-    await execa('npm', ['run', 'build'])
+    await execa('yarn', ['build'])
     // Understand if it's dist or build folder
     await execa('git', ['--work-tree', 'dist', 'add', '--all'])
     await execa('git', ['--work-tree', 'dist', 'commit', '-m', 'gh-pages'])
