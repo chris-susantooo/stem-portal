67f3fa50ec3016b89f196d6066dcd1bc80f83be6
alvinchristopher <alvinchristopher.susanto@gmail.com>
2020-05-09 10:51:16 +0800

    deployment changes

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,22 @@
+const execa = require('execa')
+const fs = require('fs');
+
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages'])
+    console.log('Building...')
+    await execa('npm', ['run', 'build'])
+    // Understand if it's dist or build folder
+    await execa('git', ['--work-tree', 'dist', 'add', '--all'])
+    await execa('git', ['--work-tree', 'dist', 'commit', '-m', 'gh-pages'])
+    console.log('Pushing to gh-pages...')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+    await fs.rmdir('dist', { recursive: true })
+    await execa('git', ['checkout', '-f', 'master'])
+    await execa('git', ['branch', '-D', 'gh-pages'])
+    console.log('Successfully deployed')
+  } catch (e) {
+    console.log(e.message)
+    process.exit(1)
+  }
+})()
